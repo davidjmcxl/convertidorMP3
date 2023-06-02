1763e39a6ab8e4a191c0a37e4885d96ecf4ed1ed
David jose moreno Castilla <91697524+davidjmcxl@users.noreply.github.com>
2023-06-02 08:16:26 -0500

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  baseUrlYoutube:"https://youtube.googleapis.com/youtube/v3/search",
-  baseUrlYoutubeMp3:"https://youtube-mp36.p.rapidapi.com/dl",
-  keyYoutube:"AIzaSyCVh_90MFtMq8erepOl4JzexLaE4lXLp4A",
-  xrapidapikey: "0ee5690910mshb752348542e677ap113a59jsne458e2dd9336",
-};

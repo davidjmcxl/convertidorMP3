715b3b81ad0d3a30e192223cb69fc98b9b045e6a
David Moreno <davidjmcxl@gmail.com>
2023-07-25 14:57:14 -0500

    cambios de apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   baseUrlYoutube:"https://youtube.googleapis.com/youtube/v3/search",
   baseUrlYoutubeMp3:"https://youtube-mp36.p.rapidapi.com/dl",
   keyYoutube:"AIzaSyCVh_90MFtMq8erepOl4JzexLaE4lXLp4A",
-  xrapidapikey: "0ee5690910mshb752348542e677ap113a59jsne458e2dd9336",
+  xrapidapikey: "eebb556fecmsh1608f63d6778102p185317jsn908ba7c11d94",
 };
 
 /*
